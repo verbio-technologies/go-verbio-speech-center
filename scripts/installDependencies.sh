6fef240990e3f56cbf479c27bbfb0032d9f120b7
Jordi Adell <jadell@verbio.com>
2025-11-27 17:42:02 +0100

    fixed typo

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 apt update
-apt install -y protobuf-compiler protoc-genc-go
+apt install -y protobuf-compiler protoc-gen-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
