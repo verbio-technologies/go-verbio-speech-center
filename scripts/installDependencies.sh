8609638fae5d583e1274a7fe561146f9c0f2d73e
Jordi Adell <jadell@verbio.com>
2025-11-27 18:50:21 +0100

    run only apt calls as sudo

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -2,8 +2,8 @@
 
 set -euxo pipefail
 
-apt update
-apt install -y protobuf-compiler protoc-gen-go
+sudo apt update
+sudo apt install -y protobuf-compiler protoc-gen-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
