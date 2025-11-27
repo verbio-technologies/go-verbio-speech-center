f75ecc1de2405b85a5cfd793c75464bd2f1194bc
Jordi Adell <jadell@verbio.com>
2025-11-27 18:32:51 +0100

    test executables in ci

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -7,3 +7,6 @@ apt install -y protobuf-compiler protoc-gen-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
+$HOME/go/bin/protoc-gen-go-grpc
+protoc-gen-go-grpc
+
