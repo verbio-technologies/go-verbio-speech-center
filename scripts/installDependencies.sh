cf2aa5c6dc59c59009dc843b795ddd58848b9418
Jordi Adell <jadell@verbio.com>
2025-11-27 18:51:31 +0100

    remove log messages

--- scripts/installDependencies.sh
+++ scripts/installDependencies.sh
@@ -7,6 +7,4 @@ sudo apt install -y protobuf-compiler protoc-gen-go
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 
-echo $HOME/go/bin/protoc-gen-go-grpc
-echo $GOBIN
 
